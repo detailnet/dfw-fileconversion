803b39ca0605de1ac81ab808d9fb4eff8a23dab6
Ivan Wolf <iwolf@detailag.ch>
2014-12-31 10:12:00 +0100

    Added Blitline module (DWS-2)

--- Adapter/BlitlineAdapter.php
+++ Adapter/BlitlineAdapter.php
@@ -2,12 +2,43 @@
 
 namespace Application\Job\Application\JobProcessing\Adapter;
 
+use Detail\Blitline\Client\BlitlineClient;
+
 class BlitlineAdapter extends BaseAdapter implements
 //    Features\Polling,
 //    Features\SynchronousProcessing,
 //    Features\AsynchronousProcessing,
     Features\Saving
 {
+    /**
+     * @var BlitlineClient
+     */
+    protected $blitlineClient;
+
+    /**
+     * @param BlitlineClient $blitlineClient
+     */
+    public function __construct(BlitlineClient $blitlineClient)
+    {
+        $this->blitlineClient = $blitlineClient;
+    }
+
+    /**
+     * @return BlitlineClient
+     */
+    public function getBlitlineClient()
+    {
+        return $this->blitlineClient;
+    }
+
+    /**
+     * @param BlitlineClient $blitlineClient
+     */
+    public function setBlitlineClient($blitlineClient)
+    {
+        $this->blitlineClient = $blitlineClient;
+    }
+
     /**
      * @param string $actionName
      * @return bool
